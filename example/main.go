3bd7a6e0fdac2750d045a6984ad71246c707c859
BeforyDeath <beforydeath@gmail.com>
2016-05-28 19:39:45 +0300

    init

---
+++ example/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/beforydeath/pagination"
+)
+
+func main() {
+
+	Paginator := pagination.Create(50, 3, 7)
+
+	//	Paginator.SetTotal(102)
+
+	pages := Paginator.Get(5)
+	fmt.Printf("First:%v\nPrev:%v\nActive:%v\nPage:%v\nNext:%v\nLast:%v\n", pages.First, pages.Prev, pages.Active, pages.Page, pages.Next, pages.Last)
+}
---
+++ pagination.go
@@ -0,0 +1,95 @@
+package pagination
+
+type Pagination struct {
+	total        int
+	limit        int
+	visibleRange int
+	current      int
+	numPages     int
+	Pages        Pages
+}
+
+type Pages struct {
+	First  int
+	Last   int
+	Active int
+	Prev   int
+	Next   int
+	Page   []int
+}
+
+func Create(total, limit, visibleRange int) *Pagination {
+
+	if limit <= 0 {
+		limit = 1
+	}
+
+	if visibleRange < 4 {
+		visibleRange = 4
+	}
+
+	p := &Pagination{0, limit, visibleRange, 0, 0, Pages{}}
+
+	p.SetTotal(total)
+
+	return p
+}
+func (p *Pagination) SetTotal(total int) {
+	if total <= 0 {
+		total = 1
+	}
+	p.total = total
+	p.setNumPages()
+	p.current = 1
+}
+
+func (p *Pagination) setNumPages() {
+	p.numPages = p.total / p.limit
+	if p.total%p.limit > 0 {
+		p.numPages++
+	}
+}
+
+func (p *Pagination) Get(current int) Pages {
+
+	if current > p.numPages {
+		// todo return error, response 404
+		current = p.numPages
+	}
+	p.current = current
+
+	firstPage := p.current - (p.visibleRange / 2)
+	if firstPage < 1 {
+		firstPage = 1
+	}
+
+	endPage := firstPage + p.visibleRange
+	if endPage > p.numPages {
+		endPage = p.numPages + 1
+	}
+
+	if firstPage > 1 {
+		p.Pages.First = 1
+	}
+
+	if p.current > 1 {
+		p.Pages.Prev = p.current - 1
+	}
+
+	p.Pages.Page = make([]int, 0)
+	for i := firstPage; i < endPage; i++ {
+		if i == p.current {
+			p.Pages.Active = i
+		}
+		p.Pages.Page = append(p.Pages.Page, i)
+	}
+
+	if p.current < p.numPages {
+		p.Pages.Next = p.current + 1
+	}
+
+	if endPage < p.numPages+1 {
+		p.Pages.Last = p.numPages
+	}
+	return p.Pages
+}
